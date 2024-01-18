d73d0c9a7b557ed61f05142409cb179364d9f898
EMarvelM <marvelousegbe19@gmail.com>
2024-01-18 15:06:16 +0100

    make sure to change database first and create only when not exists

--- dup_countries.sql
+++ dup_countries.sql
@@ -1,4 +1,5 @@
 -- To create a duplicate of an existing table called countries
+USE nation
+CREATE TABLE IF NOT EXISTS dup_countries AS SELECT * FROM countries;
 
-CREATE TABLE dup_countries AS SELECT * FROM countries;
-
+SELECT * FROM dup_countries
